60f8d8e2f8e338fb3a51ee290e980df396f73066
salepima2-blip <sale.pima2@gmail.com>
2026-07-29 10:21:25 +0700

    Add files via upload

---
+++ scripts/prefix-pages-assets.mjs
@@ -0,0 +1,45 @@
+import { readdir, readFile, stat, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+const outputDirectory = path.resolve("out");
+const repositoryPath = "/kitchenpima-blip.github.io";
+const textExtensions = new Set([".css", ".html", ".js", ".json", ".map", ".txt", ".xml"]);
+
+async function walk(directory) {
+  const entries = await readdir(directory);
+  const files = [];
+
+  for (const entry of entries) {
+    const fullPath = path.join(directory, entry);
+    const details = await stat(fullPath);
+
+    if (details.isDirectory()) {
+      files.push(...(await walk(fullPath)));
+    } else if (textExtensions.has(path.extname(entry))) {
+      files.push(fullPath);
+    }
+  }
+
+  return files;
+}
+
+function prefixPublicAssets(source) {
+  return source
+    .replaceAll('"/images/', `"${repositoryPath}/images/`)
+    .replaceAll("'/images/", `'${repositoryPath}/images/`)
+    .replaceAll('"/media/', `"${repositoryPath}/media/`)
+    .replaceAll("'/media/", `'${repositoryPath}/media/`)
+    .replaceAll('"/favicon.svg', `"${repositoryPath}/favicon.svg`)
+    .replaceAll("'/favicon.svg", `'${repositoryPath}/favicon.svg`)
+    .replaceAll('url("/images/', `url("${repositoryPath}/images/`)
+    .replaceAll("url('/images/", `url('${repositoryPath}/images/`);
+}
+
+for (const filePath of await walk(outputDirectory)) {
+  const source = await readFile(filePath, "utf8");
+  const transformed = prefixPublicAssets(source);
+
+  if (transformed !== source) {
+    await writeFile(filePath, transformed);
+  }
+}
